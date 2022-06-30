371c77351efa8bb225ce1d3628e124b2529e0244
shyju <shyju.vishu@gmail.com>
2022-06-30 18:24:32 +0530

    asd

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     base_url: "https://hasura-auth0-master.hasura.app/v1/graphql"
   },
   auth0: {
-    redirectUri: 'https://hasura-angular-auth0.herokuapp.com/home',
-    logoutRedirectUri: 'http://localhost:4200/login, https://hasura-angular-auth0.herokuapp.com/login'
+    redirectUri: 'https://hasura-angular-auth0.herokuapp.com/home;http://localhost:4200/home, ',
+    logoutRedirectUri: 'https://hasura-angular-auth0.herokuapp.com/login;http://localhost:4200/login, '
   }
 };
